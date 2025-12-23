03f06b2d0c5de1466386461680ad17e9db9aeb7a
jacobdell <jacobdell84@gmail.com>
2025-12-22 21:42:35 -0500

    Initial commit

--- my-portfolio/vite.config.js
+++ my-portfolio/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/My-Portfolio/',
   plugins: [react()],
-  base: '/My-Portfolio/'
-})
+  build: {
+    outDir: 'docs'
+  }
+});
